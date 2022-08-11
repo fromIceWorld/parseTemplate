68b88722d2b4fe9d792085500edecca8db274311
cui_bingbing <cui_bingbing@topsec.com.cn>
2022-08-11 16:47:13 +0800

    'feat:添加.d.ts输出'

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,26 @@
 import resolve from '@rollup/plugin-node-resolve';
+import dts from 'rollup-plugin-dts';
+
 import typescript from '@rollup/plugin-typescript';
-export default {
-    input: 'index.ts',
-    output: {
-        file: './dist/index.js',
-        format: 'es',
-        name: 'parseTemplate',
+export default [
+    {
+        input: 'index.ts',
+        output: {
+            file: './dist/index.js',
+            format: 'es',
+            name: 'parseTemplate',
+        },
+        strict: false,
+        plugins: [
+            resolve(),
+            typescript({
+                downlevelIteration: true,
+            }),
+        ],
     },
-    strict: false,
-    plugins: [
-        resolve(),
-        typescript({
-            downlevelIteration: true,
-        }),
-    ],
-};
+    {
+        input: 'index.ts',
+        output: [{ file: 'dist/index.d.ts', format: 'es' }],
+        plugins: [dts()],
+    },
+];

721734302af4086c88c1e5843ea90f8818769b40
cui_bingbing <cui_bingbing@topsec.com.cn>
2022-08-12 16:26:40 +0800

    feat:增加打包输出结果.d.ts，umd

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import resolve from '@rollup/plugin-node-resolve';
 import dts from 'rollup-plugin-dts';
-
 import typescript from '@rollup/plugin-typescript';
 export default [
     {
@@ -11,16 +10,39 @@ export default [
             name: 'parseTemplate',
         },
         strict: false,
+
         plugins: [
             resolve(),
             typescript({
                 downlevelIteration: true,
+                declaration: true,
+                declarationMap: true,
             }),
         ],
     },
     {
         input: 'index.ts',
-        output: [{ file: 'dist/index.d.ts', format: 'es' }],
+        output: {
+            file: './dist/index.d.ts',
+        },
         plugins: [dts()],
+    },
+    {
+        input: 'index.ts',
+        output: {
+            file: './dist/umd/index.js',
+            format: 'umd',
+            name: 'parseTemplate',
+        },
+        strict: false,
+
+        plugins: [
+            resolve(),
+            typescript({
+                downlevelIteration: true,
+                declaration: true,
+                declarationMap: true,
+            }),
+        ],
     },
 ];
